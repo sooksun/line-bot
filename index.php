50e967a3e4339954f16b87092e009b04b5d28eb3
School e office <sooksun2511@gmail.com>
2018-04-08 07:18:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Modified by Ilyasa
 */
 require_once('line_class.php');
 
-$channelAccessToken = 'c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelAccessToken = '3c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
 $channelSecret = '3163eae7704dfcf9894d608ca489bc32';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
@@ -24,9 +24,9 @@ if($message['type']=='sticker')
                                                         'replyToken' => $replyToken,							
 							'messages' => array(
 								array(
-									  "type": "text",
-									  "text": "Hello"
-								
+										'type' => 'text',									
+										'text' => 'Terima Kasih Stikernya.'										
+									
 									)
 							)
 						);
@@ -66,7 +66,7 @@ if($url['result'] != 100)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'สวัสดีครับ '.$profil->displayName.' มีอะไรให้ช่วบเหลือแจ้งมาเลยครับ.'
+										'text' => 'สวัสดีครับ '.$profil->displayName.' มีอะไรให้รับใช้ครับ'
 									)
 							)
 						);
