9d93e09eef04d504574b7443796d9cd6a2437fd0
School e office <sooksun2511@gmail.com>
2018-04-11 06:45:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,96 +1,75 @@
-<?php
-/*
-copyright @ medantechno.com
-Modified by Ilyasa
-2017
-Modified by Sooksun Sonnual
-2018
-*/
-require_once('line_class.php');
-
-$channelAccessToken = 'c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
-$channelSecret = '3163eae7704dfcf9894d608ca489bc32';//Your Channel Secret
-
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
-$userId 	= $client->parseEvents()[0]['source']['userId'];
-$replyToken = $client->parseEvents()[0]['replyToken'];
-$message 	= $client->parseEvents()[0]['message'];
-$profil = $client->profil($userId);
-$mess_text = $message['text'];
-
-if($message['type']=='sticker')
-{	
-	$callback = array(
-			'UserID' => $profil->userId,	
-                        'replyToken' => $replyToken,							
-			'messages' => array(
-					array(
-					'type' => 'sticker',									
-					'packageId' => '1' ,										
-					'stickerId' => '13'
-					)
-					)
-				);						
-}
-else
-$ask_text=str_replace(" ", "%20", $mess_text);
-$key = '0a7f12df-3ed0-4b46-985a-5d8fa72f0a1b'; //API SimSimi
-$url = 'http://api.simsimi.com/request.p?key='.$key.'&lc=th&ft=1.0&text='.$ask_text;
-
-$json_data = file_get_contents($url);
-$url=json_decode($json_data,1);
-$answer = $url['response'];
-if($message['type']=='text')
-{
-if($url['result'] == 404)
-	{
-		$callback = array(
-							'UserID' => $profil->userId,	
-                                                        'replyToken' => $replyToken,													
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => 'อืม!!!ก้อยังสงสัยอยู่นะ..'
-									)
-							)
-						);
-				
-	}
-else
-if($url['result'] != 100)
-	{
-		$callback = array(
-			'UserID' => $profil->userId,
-                        'replyToken' => $replyToken,														
-			'messages' => array(
-				array(
-					'type' => 'text',					
-					'text' => 'สวัสดีครับ '.$profil->displayName.' มีอะไรให้รับใช้ครับ'						
-				     )
-					   )
-				);
-				
-	}
-	else{
-		$callback = array(
-							'UserID' => $profil->userId,
-                                                        'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => ''.$answer.''
-									)
-							)
-						);
-						
-	}
+<?PHP
+$strAccessToken = "c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU=";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$_msg = $arrJson['events'][0]['message']['text'];
+ 
+ 
+$api_key="xC7mzpTf6-RWaaCVPjDwYxa3rwAtpvc-";
+$url = 'https://api.mlab.com/api/1/databases/tokapi/collections/autoanswer?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/tokapi/collections/autoanswer?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+$data = json_decode($json);
+$isData=sizeof($data);
+ 
+if (strpos($_msg, 'สอนว่า') !== false) {
+  if (strpos($_msg, 'สอนว่า') !== false) {
+    $x_tra = str_replace("สอนว่า","", $_msg);
+    $pieces = explode("|", $x_tra);
+    $_question=str_replace("[","",$pieces[0]);
+    $_answer=str_replace("]","",$pieces[1]);
+    //Post New Data
+    $newData = json_encode(
+      array(
+        'question' => $_question,
+        'answer'=> $_answer
+      )
+    );
+    $opts = array(
+      'http' => array(
+          'method' => "POST",
+          'header' => "Content-type: application/json",
+          'content' => $newData
+       )
+    );
+    $context = stream_context_create($opts);
+    $returnValue = file_get_contents($url,false,$context);
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนค่ะ';
+  }
+}else{
+  if($isData >0){
+   foreach($data as $rec){
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = $rec->answer;
+   }
+  }else{
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = 'ผมยังไม่เข้าใจคำถามของคุณครับ\n คุณสามารถสอนผมให้ฉลาดได้เพียงพิมพ์: สอนว่า[คำถาม|คำตอบ]';
+  }
 }
  
-$result =  json_encode($callback);
-
-file_put_contents('./reply.json',$result);
-
-
-$client->replyMessage($callback);
+ 
+$channel = curl_init();
+curl_setopt($channel, CURLOPT_URL,$strUrl);
+curl_setopt($channel, CURLOPT_HEADER, false);
+curl_setopt($channel, CURLOPT_POST, true);
+curl_setopt($channel, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($channel, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($channel, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($channel, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($channel);
+curl_close ($channel);
 ?>
