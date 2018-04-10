9be87e9878f2f80fa9001113c940fdc966f972e8
School e office <sooksun2511@gmail.com>
2018-04-10 20:40:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 copyright @ medantechno.com
 Modified by Ilyasa
 2017
+Modified by Sooksun Sonnual
+2018
 */
 require_once('line_class.php');
 
@@ -15,48 +17,52 @@ $userId 	= $client->parseEvents()[0]['source']['userId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
 $message 	= $client->parseEvents()[0]['message'];
 $profil = $client->profil($userId);
-$pesan_datang = $message['text'];
+$ans_text = $message['text'];
 
 if($message['type']=='sticker')
 {	
 	$balas = array(
-							'UserID' => $profil->userId,	
-                            'replyToken' => $replyToken,							
-							'messages' => array(
-								array(
-										'type' => 'sticker',									
-										'packageId' => '1' ,										
-										'stickerId' => '13'
-									)
-							)
-						);						
+			'UserID' => $profil->userId,	
+                        'replyToken' => $replyToken,							
+			'messages' => array(
+					array(
+					'type' => 'sticker',									
+					'packageId' => '1' ,										
+					'stickerId' => '13'
+					)
+					)
+				);						
 }
 else
-//$pesan=str_replace(" ", "%20", $pesan_datang);
-//$key = '0a7f12df-3ed0-4b46-985a-5d8fa72f0a1b'; //API SimSimi
-//$url = 'http://api.simsimi.com/request.p?key='.$key.'&lc=th&ft=1.0&text='.$pesan;
+$ask_text=str_replace(" ", "%20", $ans_text);
+$key = '0a7f12df-3ed0-4b46-985a-5d8fa72f0a1b'; //API SimSimi
+$url = 'http://api.simsimi.com/request.p?key='.$key.'&lc=th&ft=1.0&text='.$ask_text;
 
-//$json_data = file_get_contents($url);
-//$url=json_decode($json_data,1);
-//$diterima = $url['response'];
-
-$ch = curl_init();                    
-$url = 'http://banpayapraischool.ac.th/answer/find_answer.php';
-
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_POST, true);  
-curl_setopt($ch, CURLOPT_POSTFIELDS, "req=".$pesan_datang); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-$diterima = curl_exec ($ch); 
-curl_close ($ch); 
-
+$json_data = file_get_contents($url);
+$url=json_decode($json_data,1);
+$answer = $url['response'];
 if($message['type']=='text')
 {
-if($diterima == '')
+if($url['result'] == 404)
+	{
+		$balas = array(
+							'UserID' => $profil->userId,	
+                                                        'replyToken' => $replyToken,													
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'อืม!!!ก้อยังสงสัยอยู่นะ..'
+									)
+							)
+						);
+				
+	}
+else
+if($url['result'] != 100)
 	{
 		$balas = array(
 			'UserID' => $profil->userId,
-            'replyToken' => $replyToken,														
+                        'replyToken' => $replyToken,														
 			'messages' => array(
 				array(
 					'type' => 'text',					
@@ -69,11 +75,11 @@ if($diterima == '')
 	else{
 		$balas = array(
 							'UserID' => $profil->userId,
-                            'replyToken' => $replyToken,														
+                                                        'replyToken' => $replyToken,														
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => ''.$diterima.''
+										'text' => ''.$answer.''
 									)
 							)
 						);
