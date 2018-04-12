ed5611fb93b4509c1cf34f64fe1595416f0266f4
School e office <sooksun2511@gmail.com>
2018-04-12 12:26:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,8 +3,24 @@ require "vendor/autoload.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3163eae7704dfcf9894d608ca489bc32']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('U2b93ab733cb923742937b1ddc1afb328', $textMessageBuilder);
+
+    $actions =array( 
+              array( 
+                "type"=>"message",
+                "label"=> "Yes",
+                "text"=> "Yes"
+                  ),
+              array( 
+                "type"=> "message",
+                "label"=> "No",
+                "text"=> "No"
+                  )
+            );
+
+
+
+$confirmTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('hello?',$actions);
+$response = $bot->pushMessage('U2b93ab733cb923742937b1ddc1afb328', $confirmTemplateBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
