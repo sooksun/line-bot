1c37b73de049d7ce93a49acb346d5e52ce1667c9
School e office <sooksun2511@gmail.com>
2018-07-01 23:22:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -195,6 +195,6 @@ $result =  json_encode($callback);
 file_put_contents('./reply.json',$result);
 $client->replyMessage($callback);
 
-file_get_contents("http://banpayapraischool.ac.th/cron/ins_linebot.php?msg=".(ereg_replace('[[:space:]]+', '', trim($_msg)))."&user_id=".$userId."&usernames="(ereg_replace('[[:space:]]+', '', trim("นายสุขสันต์ สอนนวล"))));
+file_get_contents("http://banpayapraischool.ac.th/cron/ins_linebot.php?msg=".$_msg."&user_id=".$userId."&usernames=นายสุขสันต์สอนนวล");
 
 ?>
