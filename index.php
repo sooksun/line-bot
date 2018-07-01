0130d6ad0fe3c3660b921b84d52f2f924ff893c6
School e office <sooksun2511@gmail.com>
2018-07-01 21:55:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -191,7 +191,7 @@ if (strpos($_msg, 'สอนว่า') !== false) {
 $result =  json_encode($callback);
 file_put_contents('./reply.json',$result);
 $client->replyMessage($callback);
-file_get_contents("http://banpayapraischool.ac.th/cron/ins_linebot.php?msg=dsdflkslfjsdlfjlsdkfjlskf");
+file_get_contents("http://banpayapraischool.ac.th/cron/ins_linebot.php?msg=".$_msg);
 //file_get_contents("http://banpayapraischool.ac.th/cron/ins_linebot.php?user_id=".$profil->userId."&name=".$profil->displayName."&msg=".$_msg);
 //include ("http://banpayapraischool.ac.th/cron/ins_linebot.php?user_id=".$profil->userId."&name=".$profil->displayName."&msg=".$_msg);
 //header( "Location: http://banpayapraischool.ac.th/cron/ins_linebot.php?user_id=".$profil->userId."&name=".$profil->displayName."&msg=".$_msg);
