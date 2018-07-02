84b8665521dacf5ef26e866c0c33e8e8bdca0f1a
School e office <sooksun2511@gmail.com>
2018-07-02 07:47:07 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 $access_token = 'c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '3163eae7704dfcf9894d608ca489bc32';
 
-$pushID = $_POST["email"];  ///'U2b93ab733cb923742937b1ddc1afb328';
+$pushID = $_POST["userid"];  ///'U2b93ab733cb923742937b1ddc1afb328';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีค่ะ '.$_POST["name"].' e-mail ของคุณคือ :'.$_POST["email"]);
